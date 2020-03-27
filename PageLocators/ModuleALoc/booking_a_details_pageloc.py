49482578783dd47c5af8dffd3845040b224dc3b6
Sissixi <1071692479@qq.com>
2020-03-27 18:09:16 +0800

    Default Changelist

--- PageLocators/ModuleALoc/booking_a_details_pageloc.py
+++ PageLocators/ModuleALoc/booking_a_details_pageloc.py
@@ -4,17 +4,28 @@ Author:xixi
 Time:2020/3/26 0026 下午 8:27
 """
 from selenium.webdriver.common.by import By
+
 '''
 A端-预约订单详情-元素定位
 '''
+
+
 class Booking_detail_PageLoc:
-    #确认使用按钮
-    confirm_button=(By.ID,"reservationDetail")
-    #确认使用_弹框提示中——选择报价
-    choose_offer=(By.NAME,"choice_price")
-    #确认使用_弹框提示中——确认按钮
-    bounced_confirm_button=(By.XPATH,'//a[@class="btn_small_important weiboyiWindow_btn button"]')
-    #添加执行内容
-    add_execution_content=(By.ID,"addExecutionDocAndExecute")
-    #定位到直发内容输入框
-    straight_content=(By.XPATH,'//textarea[@class="js_content"]')
+    # 确认使用按钮
+    confirm_button = (By.ID, "reservationDetail")
+    # 确认使用_弹框提示中——选择报价
+    choose_offer = (By.NAME, "choice_price")
+    # 确认使用_弹框提示中——确认按钮
+    bounced_confirm_button = (By.XPATH, '//a[@class="btn_small_important weiboyiWindow_btn button"]')
+    # 添加执行内容
+    add_execution_content = (By.XPATH, '//div[@class="reservation_detail_operate"]//a[@action-type="add"]')
+    # 定位到直发内容输入框
+    straight_content = (By.XPATH, '//tr[@data-module="content"]/td/textarea')
+    # 是否含有视频/直播-请选择定位
+    select_live_video = (By.XPATH, '//select[@class="js_video_type"]')
+    # 执行时间—开始时间定位
+    execute_start_time = (By.ID, "start_date")
+    # 执行时间—结束时间定位
+    execute_end_time = (By.ID, "end_date")
+    # 提交执行内容
+    submit_content = (By.XPATH, '//span[text()="提交执行内容"]')
--- lianxi.py
+++ lianxi.py
@@ -12,49 +12,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
-# driver = webdriver.Chrome()
-# driver.maximize_window()
-# driver.get("http://toufang.tst-weiboyi.com:8080/\
-# pack/reservationrequirement/orderlistformedia/time_type/1\
-# /start_time//end_time//company_name//requirement_name/webzdh/requirement_id//\
-# order_id//reservation_status//order_first_review_status/10/order_execution_review_status\
-# //weibo_name//sale_manager_id//plan_manager_id//has_screenshot_with_execution_result//\
-# doc_type//customer_confirmation_status//execution_status//owner_admin_id//identity_name\
-# //is_customer_specified//is_prepayment//prepayment_status//prepayment_result//\
-# is_self_created//response_reservation_app_id//execution_operate_app_id//renege//\
-# execution_status_sub//upload_execution_img_app_id//check_spend_start_hour//\
-# check_spend_start_minute//check_spend_end_hour//check_spend_end_minute\
-# //response_spend_start_hour//response_spend_start_minute//response_spend_end_hour//response_spend_end_minute//level_id//public_advance_payment_apply_status//last_payment_status//order_by_reservation_order_checked_spend_time//order_by_response_reservation_spend_time//special_type/1/weibo_type_filter//")
-# driver.find_element(By.ID, "username").send_keys("hanyi")
-# driver.find_element(By.ID, "password").send_keys("123456")
-# driver.find_element(By.TAG_NAME, 'button').click()
-# time.sleep(10)
-# driver.find_element(By.CLASS_NAME, "AcceptReservationBrief").click()
-# time.sleep(1)
-# # 1.实例化select类--提供select定位元素对象
-# loc = (By.ID, 'with_topic_and_link')
-# # 等待select元素可见
-# WebDriverWait(driver, 10).until(EC.visibility_of_element_located(loc))
-# # 找到select元素
-# select_ele = driver.find_element(*loc)
-# # 实例化select类
-# s = Select(select_ele)
-# time.sleep(1)
-# s.select_by_visible_text('是')
-# time.sleep(1)
-# #应约按钮
-# driver.find_element(By.XPATH, '//*[@id="weiboyiCmp79"]//label[@class="replay_reservation"]').click()
-# time.sleep(1)
-#
-# body = "嘻嘻嘻哈哈哈啊"
-#
-# # js处理iframe问题
-# js = 'document.getElementById("ueditor_0").contentWindow.document.body.innerHTML="{}"'.format(body)
-# driver.execute_script(js)
-# time.sleep(2)
-# #提交按钮
-# driver.find_element(By.XPATH, '//*[contains(@id,"weiboyiCmp")]//a[contains(@class,"btn_small_important")]').click()
-# time.sleep(2)
-#
-# driver.quit()
-print(time.strftime("%Y-%m-%d"))
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("http://chuanbo.tst-weiboyi.com/hwreservation/order/detail/id/58551#anchor_add_execution_content")
+driver.find_element(By.ID, "username").send_keys("luckyxi")
+driver.find_element(By.ID, "password").send_keys("wbyxixi123")
+driver.find_element(By.ID, "piccode").send_keys("1234")
+driver.find_element(By.CLASS_NAME, 'btn_wrap').click()
+
+# 添加执行内容
+loc = (By.XPATH, '//div[@class="reservation_detail_operate"]//a[@action-type="add"]')
+WebDriverWait(driver, 20).until(EC.visibility_of_element_located(loc))
+driver.find_element(*loc).click()
+time.sleep(1)
+loc = (By.XPATH, '//tr[@data-module="content"]/td/textarea')
+driver.find_element(*loc).click()
+time.sleep(1)
+js1 = 'var a =document.getElementsByClassName("js_content")[0];a.value="xixixixi"'
+driver.execute_script(js1)
+time.sleep(10)
+
+driver.quit()
